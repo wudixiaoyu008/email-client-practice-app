7da7431fca88b4ac6c861bd6912f1447fe181074
Yu Liu <liuyuc@outlook.com>
2018-01-28 22:59:15 -0500

    add style & refactor & mailbox

--- src/index.js
+++ src/index.js
@@ -1,66 +1,89 @@
 /**
  * @flow
  */
-// "Thu, 7 Dec 2017 10:20:34 +0500"
-function formatDate(str) {
-  const monthListA = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-  const monthList1 = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
-  let format = str.split(' ');
-  format[2] = monthList1[monthListA.indexOf(format[2])];
-  return `${format[2]}/${format[1]}/${format[3]} ${format[4]}`;
-}
-
 const store = require('./store/emails.json');
 
-const inboxId = store.mailboxes.INBOX.threadIds;
-
-// email id map to email object
-const inboxList = inboxId.map((id) => {
-  const emailObject = {};
-  const email = store.messages[id];
-  const emailHeader = store.messages[id].payload.headers;
-
-  emailObject.sender = emailHeader[1].value.replace(/\s\\.+/g, '');
-  // emailObject.time = emailHeader[0].value;
-  const time = emailHeader[0].value;
-  emailObject.time = formatDate(time);
-  emailObject.subject = emailHeader[emailHeader.length - 1].value;
-  emailObject.snippet = email.snippet;
-  return emailObject;
-});
-
-// email object map to list item html
-const inboxHtmlList = inboxList.map((email) => {
-  const li = `
-      <li>
-        <button class="email-item" type="button">
-          <div class="sender-details">
-            <p> ${email.sender} </p>
-            <span>${email.time}</span>
-          </div>
-          <p class="email-subject">${email.subject}</p>
-          <p class="email-snippet">${email.snippet}</p>
-        </button>
-      </li>`;
-  return li;
-});
-
-
-function renderSidebar() {
-  let liHtml = '';
-  for (const item of inboxHtmlList) {
-    liHtml += item;
-  }
-
-  let sidebarContents = `
-      <h2 class="email-header">Inbox</h2>
-      <ul class="email-list">
-        ${liHtml}
-      </ul>`;
-
-  const container = document.querySelector('.email-list-container');
-
-  if (container != null) container.innerHTML = sidebarContents;
+function formatMailbox(mailbox) {
+  const category = mailbox.replace(/.+_/, '');
+  const lowerCase = [...category].map((letter, index) => index === 0 ?
+    letter.toUpperCase() : letter.toLowerCase());
+  return lowerCase.join('');
 }
 
-renderSidebar();
+function renderMailbox() {
+  const mailboxes = Object.keys(store.mailboxes);
+  const mailboxHTML = mailboxes.map((mailbox, index) => {
+    const category = formatMailbox(mailbox);
+    return index === 0 ? `
+        <li class="mailbox-item active">${category}</li>
+      ` : `
+        <li class="mailbox-item">${category}</li>
+      `;
+  });
+  const mailboxList = mailboxHTML.join('');
+  const container = document.querySelector('.mailbox-list');
+  if (container != null) container.innerHTML = mailboxList;
+}
+
+function formatDate(time) {
+  const date = new Date(Number(time));
+  return date.toLocaleString('en-US', {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+  });
+}
+
+function formatSubject(subject) {
+  return subject.length > 45 ? `${subject.substring(0, 45)}...` : subject;
+}
+
+function formatSnippet(snippet) {
+  return snippet.length > 110 ? `${snippet.substring(0, 110)}...` : snippet;
+}
+
+function getLastMessage(thread) {
+  const [lastMessage] = thread.messages.slice(-1); // slice -> array
+  return store.messages[lastMessage.id];
+}
+
+function renderThreads() {
+  const inboxId = store.mailboxes.INBOX.threadIds;
+
+  // email id map to email object
+  const inboxList = inboxId.map((id) => {
+    const thread = store.threads[id];
+    const email = getLastMessage(thread);
+    const emailHeader = email.payload.headers;
+    const time = email.internalDate;
+    return {
+      sender: emailHeader[1].value.replace(/\s\\.+/g, '') || 'No Sender',
+      time: formatDate(time),
+      subject: emailHeader[emailHeader.length - 1].value || 'No Subject',
+      snippet: email.snippet,
+    };
+  });
+
+  // email object to HTML
+  const inboxHtmlList = inboxList.map(email => `
+    <li>
+      <button class="email-item" type="button">
+        <div class="sender-details">
+          <p> ${email.sender} </p>
+          <span>${email.time}</span>
+        </div>
+        <p class="email-subject">${formatSubject(email.subject)}</p>
+        <p class="email-snippet">${formatSnippet(email.snippet)}</p>
+      </button>
+    </li>
+  `);
+
+  const liHtml = inboxHtmlList.join('');
+  const container = document.querySelector('.email-list');
+  if (container != null) container.innerHTML = liHtml;
+}
+
+renderMailbox();
+renderThreads();
